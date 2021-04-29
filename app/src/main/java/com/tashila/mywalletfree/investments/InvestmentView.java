236fb8030d4a8ba72e9656aac8ba0e4677c5d782
Tashila <tashila.pathum@gmail.com>
2021-04-29 16:18:45 +0530

    collapsing layout

--- app/src/main/java/com/tashila/mywalletfree/investments/InvestmentView.java
+++ app/src/main/java/com/tashila/mywalletfree/investments/InvestmentView.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.google.android.material.transition.MaterialSharedAxis;
+import com.tashila.mywalletfree.R;
 
 public class InvestmentView extends Fragment {
 
@@ -24,6 +29,27 @@ public class InvestmentView extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_investment_view, container, false);
+
+        LinearLayout collapsingLayout = view.findViewById(R.id.collapsing_layout);
+        ObservableScrollView observableScrollView = view.findViewById(R.id.observable_scroll_view);
+        observableScrollView.setScrollViewCallbacks(new ObservableScrollViewCallbacks() {
+            @Override
+            public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+                collapsingLayout.setTranslationY(scrollY / 2);
+            }
+
+            @Override
+            public void onDownMotionEvent() {
+
+            }
+
+            @Override
+            public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+
+            }
+        });
+
+        return view;
     }
 }
