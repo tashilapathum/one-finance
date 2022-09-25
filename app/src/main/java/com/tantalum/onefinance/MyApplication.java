a0c46a29cbf34f0243ec4ace18ca62711a3ecd1c
Tashila Pathum <tashila.pathum@gmail.com>
2022-09-25 10:53:59 +0530

    added CAOC

---
+++ app/src/main/java/com/tantalum/onefinance/MyApplication.java
@@ -0,0 +1,18 @@
+package com.tantalum.onefinance;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+import cat.ereza.customactivityoncrash.config.CaocConfig;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CaocConfig.Builder.create().errorDrawable(R.drawable.ic_bug).apply();
+        FirebaseApp.initializeApp(getApplicationContext());
+    }
+}
